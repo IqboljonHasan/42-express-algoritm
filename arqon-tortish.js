53281691351bd12ef522883f07dae2ee4ce2f60c
Iqboljon Hasan <45452823+IqboljonHasan@users.noreply.github.com>
2025-03-27 15:39:43 +0500

    Update arqon-tortish.js

--- arqon-tortish.js
+++ arqon-tortish.js
@@ -1,4 +1,4 @@
-function leftRightDifference(nums) {
+function leftRightDifferenceOn2(nums) {
     let res = [], l = 0, r = 0
     for (let i = 0; i < nums.length; i++) {
         l = 0
@@ -16,4 +16,25 @@ function leftRightDifference(nums) {
     return res
 }
 
-module.exports = { leftRightDifference }
+function leftRightDifferenceOn(nums) {
+    let res = [], l = 0, r = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (i == 0) {
+            for (let j = 0; j < nums.length; j++) {
+                if (j < i) {
+                    l += nums[j]
+                } else if (j > i) {
+                    r += nums[j]
+                }
+            }
+        }
+        else {
+            l += nums[i-1]
+            r -= nums[i]
+        }
+        res[i] = r == l ? 0 : l > r ? -1 : 1
+    }
+
+    return res
+}
+
